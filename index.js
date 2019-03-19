d212c0290fbbd6b5dda84073d0081a1d5953c0e0
Fakhir Shad <fakhir.shad@hotmail.com>
2019-03-19 14:47:05 +0500

    #fixing imports

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 var Cookies = require("js-cookie");
 
-exports.create = function(flash = {}) {
-  if (Object.values(flash).filter(obj => obj).length > 0) {
-    var name = uniqueName();
-    storeName(name);
-    Cookies.set(name, flash);
-  }
-};
-
 function uniqueName(name = getName()) {
   return Cookies.get(name) === undefined ? name : uniqueName(getName());
 }
@@ -23,17 +15,26 @@ function storeName(name) {
   Cookies.set("jsFlashNames", names);
 }
 
-exports.get = function() {
-  var storedFlash = Cookies.getJSON("jsFlashNames");
-  if (storedFlash === undefined) {
-    return {};
-  } else {
-    var flashes = storedFlash.map(function(flashName) {
-      var flash = Cookies.getJSON(flashName);
-      Cookies.remove(flashName);
-      return flash;
-    });
-    Cookies.remove("jsFlashNames");
-    return flashes;
+module.exports = {
+  create: function(flash = {}) {
+    if (Object.values(flash).filter(obj => obj).length > 0) {
+      var name = uniqueName();
+      storeName(name);
+      Cookies.set(name, flash);
+    }
+  },
+  get: function() {
+    var storedFlash = Cookies.getJSON("jsFlashNames");
+    if (storedFlash === undefined) {
+      return {};
+    } else {
+      var flashes = storedFlash.map(function(flashName) {
+        var flash = Cookies.getJSON(flashName);
+        Cookies.remove(flashName);
+        return flash;
+      });
+      Cookies.remove("jsFlashNames");
+      return flashes;
+    }
   }
 };
