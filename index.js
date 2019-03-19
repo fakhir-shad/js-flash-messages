2ab92c22a119c2bde3608915d998a2d77b5e4ae6
Fakhir Shad <fakhir.shad@hotmail.com>
2019-03-19 15:31:26 +0500

    #removed src

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-const Flash = require("./src/flash");
+const Cookies = require("js-cookie");
 
-module.exports = Flash;
+const create = (flash = {}) => {
+  if (Object.values(flash).filter(obj => obj).length > 0) {
+    const name = uniqueName();
+    storeName(name);
+    Cookies.set(name, flash);
+  }
+};
+
+const uniqueName = (name = getName()) => {
+  return Cookies.get(name) === undefined ? name : uniqueName(getName());
+};
+
+const getName = () => {
+  return `jsFlash-${Date.now()}`;
+};
+
+const storeName = name => {
+  const storedNames = Cookies.getJSON("jsFlashNames");
+  const names = storedNames === undefined ? [] : storedNames;
+  names.push(name);
+  Cookies.set("jsFlashNames", names);
+};
+
+const get = () => {
+  const storedFlash = Cookies.getJSON("jsFlashNames");
+  if (storedFlash === undefined) {
+    return [];
+  } else {
+    const flashes = storedFlash.map(flashName => {
+      const flash = Cookies.getJSON(flashName);
+      Cookies.remove(flashName);
+      return flash;
+    });
+    Cookies.remove("jsFlashNames");
+    return flashes;
+  }
+};
+
+exports.create = create;
+exports.get = get;
